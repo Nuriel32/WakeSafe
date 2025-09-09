845bc99f4d17e8347777dbaced61969d25765e31
Nuriel32 <nuriel.32@gmail.com>
2025-09-09 23:42:23 +0300

    fixing ratelimit issues

--- server/middlewares/rateLimit.js
+++ server/middlewares/rateLimit.js
@@ -1,16 +1,11 @@
-// server/middlewares/rateLimit.js
+// middlewares/rateLimit.js
 const rateLimit = require('express-rate-limit');
 
-function buildRateLimiter() {
-  return rateLimit({
-    windowMs: 60 * 1000,          // 1 minute
-    max: 60,                      // 60 req/min per IP
-    standardHeaders: true,        // RateLimit-* headers
-    legacyHeaders: false,         // no X-RateLimit-*
-    keyGenerator: (req) => req.ip // uses trust proxy if enabled
-    // If you *really* want to silence validations:
-    // validate: { xForwardedForHeader: false },
-  });
-}
+const rateLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 60,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
-module.exports = { buildRateLimiter };
+module.exports = rateLimiter; // <-- export the function instance
