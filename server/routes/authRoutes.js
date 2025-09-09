77b75d335baae568c9984e4989e2d39368fe8458
Nuriel32 <nuriel.32@gmail.com>
2025-09-09 23:52:10 +0300

    updating ratelimiter

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middlewares/auth');
-const rateLimiter = require('../middlewares/rateLimit');
+const { authLimiter } = require('../middlewares/rateLimit');
 
 // הרשמה והתחברות
-router.post('/register', rateLimiter, authController.register);
-router.post('/login', rateLimiter, authController.login);
+router.post('/register', authLimiter, authController.register);
+router.post('/login', authLimiter, authController.login);
 router.post('/logout', auth, authController.logout);
 
 module.exports = router;
